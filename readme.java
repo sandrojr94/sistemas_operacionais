262b048c4c18c17a5f58ad70fbcffe338864e2b0
sandrojr94 <79672288+sandrojr94@users.noreply.github.com>
2021-02-25 20:34:52 -0300

    Update readme.java

--- readme.java
+++ readme.java
@@ -1,7 +1,7 @@
 # sistemas_operacionais
 Repositório para utilizar nas aulas de Sistemas Operacionais.
 
-class Main {
+class readme {
   public static void main(String[] args) {
     System.out.println("Olá, meu nome é Sandro, moro em Bebedouro-SP");
   }
