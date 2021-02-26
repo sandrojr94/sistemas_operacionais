218bd7a0ad2fa7205e9f4f910ceb9a54f620b6ed
sandrojr94 <79672288+sandrojr94@users.noreply.github.com>
2021-02-25 21:20:01 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ Repositório para utilizar nas aulas de Sistemas Operacionais.
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Olá, meu nome é Sandro, moro em Bebedouro-SP");
+    System.out.println("Olá, meu nome é Sandro, moro em Bebedouro SP");
   }
 }
